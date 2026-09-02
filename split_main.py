b47585f21cca9fefaa6e7a2ab739e800c0dada9b
dimzragil <kiritotamvan48@gmail.com>
2026-09-02 20:52:45 +0700

    chore: remove leftover python scripts

--- split_main.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open("src/main.rs", "r") as f:
-    content = f.read()
-
-# We want to extract the Event::Input handling and Event::Action handling.
-# This might be tricky with regex due to nested braces.
